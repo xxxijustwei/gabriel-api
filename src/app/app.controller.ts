ef39da30565603ff33a45899efc122d841688192
justwei <admin@taylorswiftcn.com>
2025-05-01 20:53:57 +0800

    refactor: implement streaming for funding flow analysis
    - Modify getFundingFlowAnalyze to support streaming

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -8,7 +8,12 @@ import {
     Res,
     UseGuards,
 } from "@nestjs/common";
-import { convertToCoreMessages, smoothStream, streamText } from "ai";
+import {
+    convertToCoreMessages,
+    pipeDataStreamToResponse,
+    smoothStream,
+    streamText,
+} from "ai";
 import type { Response } from "express";
 import { OnlyCronJobGuard } from "../guards/only-corn-job";
 import { getFundingFlowAnalyze } from "../lib/tools/get-funding-flow-analyze";
@@ -39,28 +44,32 @@ export class AppController {
 
     @Post("api/chat")
     async chat(@Res() res: Response, @Body() { messages }: ChatBody) {
-        const result = streamText({
-            model: xai("grok-3-fast"),
-            system: "你是一个非常专业的加密货币交易员，擅长分析加密货币市场的数据，并给出交易建议。",
-            tools: {
-                getTokenFundingRate,
-                getFundingRateWithLimitAndSort,
-                getFundingFlowAnalyze,
-                getTaskConfig,
-                setTaskConfig,
-            },
-            maxSteps: 5,
-            messages: convertToCoreMessages(messages),
-            experimental_transform: smoothStream({
-                chunking: /[\u4E00-\u9FFF]|\S+\s+/,
-            }),
-            onError: (error) => {
-                this.logger.error(handleError(error));
-            },
-        });
+        pipeDataStreamToResponse(res, {
+            execute: async (dataStream) => {
+                const result = streamText({
+                    model: xai("grok-3-fast"),
+                    system: "你是一个非常专业的加密货币交易员，擅长分析加密货币市场的数据，并给出交易建议。",
+                    messages: convertToCoreMessages(messages),
+                    tools: {
+                        getTokenFundingRate,
+                        getFundingRateWithLimitAndSort,
+                        getFundingFlowAnalyze: getFundingFlowAnalyze({
+                            dataStream,
+                        }),
+                        getTaskConfig,
+                        setTaskConfig,
+                    },
+                    experimental_transform: smoothStream({
+                        chunking: /[\u4E00-\u9FFF]|\S+\s+/,
+                    }),
+                });
 
-        result.pipeDataStreamToResponse(res, {
-            getErrorMessage: handleError,
+                result.mergeIntoDataStream(dataStream);
+            },
+            onError: (error) => {
+                const mesage = handleError(error);
+                return mesage;
+            },
         });
     }
 
--- src/lib/tools/get-funding-flow-analyze.ts
+++ src/lib/tools/get-funding-flow-analyze.ts
@@ -1,3 +1,5 @@
+import { xai } from "@ai-sdk/xai";
+import { type DataStreamWriter, smoothStream, streamText } from "ai";
 import dayjs from "dayjs";
 import { z } from "zod";
 import { analyzeFundingFlow } from "../binance/analyze-funding-flow";
@@ -28,26 +30,60 @@ const parameters = z.object({
         .describe("Limit on the number of K lines (optional)"),
 });
 
-const execute = async ({
-    symbol = "BTC",
-    interval = "15m",
-    klinesLimit = 48,
-}: z.infer<typeof parameters>) => {
-    const result = await analyzeFundingFlow({
-        symbol: symbol.endsWith("USDT") ? symbol : `${symbol}USDT`,
-        interval,
-        limit: klinesLimit,
-    });
+export const getFundingFlowAnalyze = ({
+    dataStream,
+}: { dataStream: DataStreamWriter }) => {
+    return {
+        description,
+        parameters,
+        execute: async ({
+            symbol = "BTC",
+            interval = "15m",
+            klinesLimit = 48,
+        }: z.infer<typeof parameters>) => {
+            const result = await analyzeFundingFlow({
+                symbol: symbol.endsWith("USDT") ? symbol : `${symbol}USDT`,
+                interval,
+                limit: klinesLimit,
+            });
 
-    if (!result) {
-        return "没有找到数据,无法进行分析";
-    }
+            if (!result) {
+                return "没有找到数据,无法进行分析";
+            }
 
-    return getPrompt(result);
-};
+            let streamResult = "";
+            const { fullStream } = streamText({
+                model: xai("grok-3-fast"),
+                system: "你是一个非常专业的加密货币交易员，擅长分析加密货币市场的数据，并给出交易建议。",
+                prompt: getPrompt(result),
+                experimental_transform: smoothStream({
+                    chunking: /[\u4E00-\u9FFF]|\S+\s+/,
+                }),
+            });
 
-export const getFundingFlowAnalyze = {
-    description,
-    parameters,
-    execute,
+            dataStream.writeData({
+                type: "clear",
+                content: "",
+            });
+
+            for await (const chunk of fullStream) {
+                const { type } = chunk;
+                if (type !== "text-delta") continue;
+
+                const { textDelta } = chunk;
+                streamResult += textDelta;
+                dataStream.writeData({
+                    type: "text-delta",
+                    content: textDelta,
+                });
+            }
+
+            dataStream.writeData({
+                type: "finish",
+                content: "",
+            });
+
+            return streamResult;
+        },
+    };
 };
